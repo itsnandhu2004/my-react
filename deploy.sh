f523d9e5c1197f59ba86e02b96b3913734883cdf
itsnandhu2004 <nandhini.info2004@gmail.com>
2025-02-06 15:25:02 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,26 @@
 #!/bin/bash
-echo run
+# Print a message indicating the start of the process
+echo "Running deployment process..."
+
+# Grant execute permissions to the build script
 chmod 777 build.sh
+
+# Run the build script
 ./build.sh
+
+# Log in to Docker using the credentials
 docker login -u nandhini1694 -p dckr_pat_08XhH0G6oTntx5pCb0hPNQVtQ1k
+
+# Tag the Docker image with your repository
 docker tag port nandhini1694/portfolio
+
+# Push the image to Docker Hub
 docker push nandhini1694/portfolio
+
+# Apply Kubernetes deployment using kubectl
+echo "Deploying to Kubernetes..."
+kubectl apply -f k8s-deployment.yaml
+
+# Check the status of the Kubernetes deployment
+kubectl get pods
+kubectl get svc
